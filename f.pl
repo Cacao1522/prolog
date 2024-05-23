ba53c28df897daa5932787fbc8c2d610addeaed9
Cacao1522 <kaorukato2003@gmail.com>
2024-05-23 12:55:19 +0900

    a

--- f.pl
+++ f.pl
@@ -3,5 +3,9 @@ p(a).
 3.
 p(t(b)).
 parent(pam,bob).
+parent(tom,bob).
+parent(p(a),t(b)).
+bob.
+p(S).
 mem(X,[X | Tail]).
 mem(X, [Head | Tail]) :- mem(X, Tail).
--- rep06.pl
+++ rep06.pl
@@ -35,14 +35,205 @@ naf(_).
       が阻止されてしまう）．
 */
 findallterm(Term) :- read(CurrentTerm),process(CurrentTerm,Term).
-process(end_of_file,_) :- !.
-process(CurrentTerm,Term) :- (naf(CurrentTerm=Term),!;write(CurrentTerm),nl),findallterm(Term).
+process(end_of_file,_) :- !. % 読んだ項がend_of_fileであればカットして終了
+process(CurrentTerm,Term) :- (naf(CurrentTerm=Term),!; % 項がマッチしないならカット
+                            write(CurrentTerm),nl), % 項がマッチするなら現在の項を出力し、出力を改行する
+                            findallterm(Term). % 項がマッチしてもしなくても再びfindallterm(Term)を呼び出す
 
-/*（実行例）
+/*（ファイルfの内容）
+apple.
+p(a).
+3.
+p(t(b)).
+parent(pam,bob).
+parent(tom,bob).
+parent(p(a),t(b)).
+bob.
+p(S).
+mem(X,[X | Tail]).
+mem(X, [Head | Tail]) :- mem(X, Tail).
 
+（実行例）
+?- findallterm(X).
+apple
+p(a)
+3
+p(t(b))
+parent(pam,bob)
+parent(tom,bob)
+parent(p(a),t(b))
+bob
+p(_91138)
+mem(_91146,[_91146|_91148])
+mem(_91168,[_91162|_91164]):-mem(_91168,_91164)
+true.
 
+?- findallterm(p(X)).
+p(a)
+p(t(b))
+p(_95662)
+true.
+
+?- findallterm(p(t(X))).
+p(t(b))
+p(_4502)
+true.
+
+?- findallterm(parent(p(a),t(bob))).
+true.
+
+?- findallterm(mem(a,[a,b])).
+mem(_26844,[_26844|_26846])
+true.
+
+[trace]  ?- findallterm(parent(X,bob)).
+   Call: (12) findallterm(parent(_32888, bob)) ? creep
+   Call: (13) read(_34226) ? creep
+   Exit: (13) read(apple) ? creep
+   Call: (13) process(apple, parent(_32888, bob)) ? creep
+   Call: (14) naf(apple=parent(_32888, bob)) ? creep
+   Call: (15) apple=parent(_32888, bob) ? creep
+   Fail: (15) apple=parent(_32888, bob) ? creep
+   Redo: (14) naf(apple=parent(_32888, bob)) ? creep
+   Exit: (14) naf(apple=parent(_32888, bob)) ? creep
+   Call: (14) findallterm(parent(_32888, bob)) ? creep
+   Call: (15) read(_41500) ? creep
+   Exit: (15) read(p(a)) ? creep
+   Call: (15) process(p(a), parent(_32888, bob)) ? creep
+   Call: (16) naf(p(a)=parent(_32888, bob)) ? creep
+   Call: (17) p(a)=parent(_32888, bob) ? creep
+   Fail: (17) p(a)=parent(_32888, bob) ? creep
+   Redo: (16) naf(p(a)=parent(_32888, bob)) ? creep
+   Exit: (16) naf(p(a)=parent(_32888, bob)) ? creep
+   Call: (16) findallterm(parent(_32888, bob)) ? creep
+   Call: (17) read(_48778) ? creep
+   Exit: (17) read(3) ? creep
+   Call: (17) process(3, parent(_32888, bob)) ? creep
+   Call: (18) naf(3=parent(_32888, bob)) ? creep
+   Call: (19) 3=parent(_32888, bob) ? creep
+   Fail: (19) 3=parent(_32888, bob) ? creep
+   Redo: (18) naf(3=parent(_32888, bob)) ? creep
+   Exit: (18) naf(3=parent(_32888, bob)) ? creep
+   Call: (18) findallterm(parent(_32888, bob)) ? creep
+   Call: (19) read(_56052) ? creep
+   Exit: (19) read(p(t(b))) ? creep
+   Call: (19) process(p(t(b)), parent(_32888, bob)) ? creep
+   Call: (20) naf(p(t(b))=parent(_32888, bob)) ? creep
+   Call: (21) p(t(b))=parent(_32888, bob) ? creep
+   Fail: (21) p(t(b))=parent(_32888, bob) ? creep
+   Redo: (20) naf(p(t(b))=parent(_32888, bob)) ? creep
+   Exit: (20) naf(p(t(b))=parent(_32888, bob)) ? creep
+   Call: (20) findallterm(parent(_32888, bob)) ? creep
+   Call: (21) read(_63334) ? creep
+   Exit: (21) read(parent(pam, bob)) ? creep
+   Call: (21) process(parent(pam, bob), parent(_58, bob)) ? creep
+   Call: (22) naf(parent(pam, bob)=parent(_58, bob)) ? creep
+   Call: (23) parent(pam, bob)=parent(_58, bob) ? creep
+   Exit: (23) parent(pam, bob)=parent(pam, bob) ? creep
+   Call: (23) fail ? creep
+   Fail: (23) fail ? creep
+   Fail: (22) naf(parent(pam, bob)=parent(_58, bob)) ? creep
+   Redo: (21) process(parent(pam, bob), parent(_58, bob)) ? creep
+   Call: (22) write(parent(pam, bob)) ? creep
+parent(pam,bob)
+   Exit: (22) write(parent(pam, bob)) ? creep
+   Call: (22) nl ? creep
+
+   Exit: (22) nl ? creep
+   Call: (22) findallterm(parent(_58, bob)) ? creep
+   Call: (23) read(_11316) ? creep
+   Exit: (23) read(parent(tom, bob)) ? creep
+   Call: (23) process(parent(tom, bob), parent(_58, bob)) ? creep
+   Call: (24) naf(parent(tom, bob)=parent(_58, bob)) ? creep
+   Call: (25) parent(tom, bob)=parent(_58, bob) ? creep
+   Exit: (25) parent(tom, bob)=parent(tom, bob) ? creep
+   Call: (25) fail ? creep
+   Fail: (25) fail ? creep
+   Fail: (24) naf(parent(tom, bob)=parent(_58, bob)) ? creep
+   Redo: (23) process(parent(tom, bob), parent(_58, bob)) ? creep
+   Call: (24) write(parent(tom, bob)) ? creep
+parent(tom,bob)
+   Exit: (24) write(parent(tom, bob)) ? creep
+   Call: (24) nl ? creep
+
+   Exit: (24) nl ? creep
+   Call: (24) findallterm(parent(_58, bob)) ? creep
+   Call: (25) read(_23404) ? creep
+   Exit: (25) read(parent(p(a), t(b))) ? creep
+   Call: (25) process(parent(p(a), t(b)), parent(_58, bob)) ? creep
+   Call: (26) naf(parent(p(a), t(b))=parent(_58, bob)) ? creep
+   Call: (27) parent(p(a), t(b))=parent(_58, bob) ? creep
+   Fail: (27) parent(p(a), t(b))=parent(_58, bob) ? creep
+   Redo: (26) naf(parent(p(a), t(b))=parent(_58, bob)) ? creep
+   Exit: (26) naf(parent(p(a), t(b))=parent(_58, bob)) ? creep
+   Call: (26) findallterm(parent(_58, bob)) ? creep
+   Call: (27) read(_30692) ? creep
+   Exit: (27) read(bob) ? creep
+   Call: (27) process(bob, parent(_58, bob)) ? creep
+   Call: (28) naf(bob=parent(_58, bob)) ? creep
+   Call: (29) bob=parent(_58, bob) ? creep
+   Fail: (29) bob=parent(_58, bob) ? creep
+   Redo: (28) naf(bob=parent(_58, bob)) ? creep
+   Exit: (28) naf(bob=parent(_58, bob)) ? creep
+   Call: (28) findallterm(parent(_58, bob)) ? creep
+   Call: (29) read(_37966) ? creep
+   Exit: (29) read(p(_38776)) ? creep
+   Call: (29) process(p(_38776), parent(_58, bob)) ? creep
+   Call: (30) naf(p(_38776)=parent(_58, bob)) ? creep
+   Call: (31) p(_38776)=parent(_58, bob) ? creep
+   Fail: (31) p(_38776)=parent(_58, bob) ? creep
+   Redo: (30) naf(p(_38776)=parent(_58, bob)) ? creep
+   Exit: (30) naf(p(_38776)=parent(_58, bob)) ? creep
+   Call: (30) findallterm(parent(_58, bob)) ? creep
+   Call: (31) read(_45244) ? creep
+   Exit: (31) read(mem(_46056, [_46056|_46058])) ? creep
+   Call: (31) process(mem(_46056, [_46056|_46058]), parent(_58, bob)) ? creep
+   Call: (32) naf(mem(_46056, [_46056|_46058])=parent(_58, bob)) ? creep
+   Call: (33) mem(_46056, [_46056|_46058])=parent(_58, bob) ? creep
+   Fail: (33) mem(_46056, [_46056|_46058])=parent(_58, bob) ? creep
+   Redo: (32) naf(mem(_46056, [_46056|_46058])=parent(_58, bob)) ? creep
+   Exit: (32) naf(mem(_46056, [_46056|_46058])=parent(_58, bob)) ? creep
+   Call: (32) findallterm(parent(_58, bob)) ? creep
+   Call: (33) read(_52532) ? creep
+   Exit: (33) read((mem(_53350, [_53344|_53346]):-mem(_53350, _53346))) ? creep
+   Call: (33) process((mem(_53350, [_53344|_53346]):-mem(_53350, _53346)), parent(_58, bob)) ? creep
+   Call: (34) naf((mem(_53350, [_53344|_53346]):-mem(_53350, _53346))=parent(_58, bob)) ? creep
+   Call: (35) (mem(_53350, [_53344|_53346]):-mem(_53350, _53346))=parent(_58, bob) ? creep
+   Fail: (35) (mem(_53350, [_53344|_53346]):-mem(_53350, _53346))=parent(_58, bob) ? creep
+   Redo: (34) naf((mem(_53350, [_53344|_53346]):-mem(_53350, _53346))=parent(_58, bob)) ? creep
+   Exit: (34) naf((mem(_53350, [_53344|_53346]):-mem(_53350, _53346))=parent(_58, bob)) ? creep
+   Call: (34) findallterm(parent(_58, bob)) ? creep
+   Call: (35) read(_59832) ? creep
+   Exit: (35) read(end_of_file) ? creep
+   Call: (35) process(end_of_file, parent(_58, bob)) ? creep
+   Exit: (35) process(end_of_file, parent(_58, bob)) ? creep
+   Exit: (34) findallterm(parent(_58, bob)) ? creep
+   Exit: (33) process((mem(_208, [_202|_204]):-mem(_208, _204)), parent(_58, bob)) ? creep
+   Exit: (32) findallterm(parent(_58, bob)) ? creep
+   Exit: (31) process(mem(_186, [_186|_188]), parent(_58, bob)) ? creep
+   Exit: (30) findallterm(parent(_58, bob)) ? creep
+   Exit: (29) process(p(_178), parent(_58, bob)) ? creep
+   Exit: (28) findallterm(parent(_58, bob)) ? creep
+   Exit: (27) process(bob, parent(_58, bob)) ? creep
+   Exit: (26) findallterm(parent(_58, bob)) ? creep
+   Exit: (25) process(parent(p(a), t(b)), parent(_58, bob)) ? creep
+   Exit: (24) findallterm(parent(_58, bob)) ? creep
+   Exit: (23) process(parent(tom, bob), parent(_58, bob)) ? creep
+   Exit: (22) findallterm(parent(_58, bob)) ? creep
+   Exit: (21) process(parent(pam, bob), parent(_58, bob)) ? creep
+   Exit: (20) findallterm(parent(_58, bob)) ? creep
+   Exit: (19) process(p(t(b)), parent(_58, bob)) ? creep
+   Exit: (18) findallterm(parent(_58, bob)) ? creep
+   Exit: (17) process(3, parent(_58, bob)) ? creep
+   Exit: (16) findallterm(parent(_58, bob)) ? creep
+   Exit: (15) process(p(a), parent(_58, bob)) ? creep
+   Exit: (14) findallterm(parent(_58, bob)) ? creep
+   Exit: (13) process(apple, parent(_58, bob)) ? creep
+   Exit: (12) findallterm(parent(_58, bob)) ? creep
+true.
 (説明)
-
+まずreadで項を読み、その後処理processを実行する。処理processでは読んだ項がend_of_fileであればカットして終了する。
+findalltermの引数Termに変数を入れるとその変数にend_of_fileが代入されて終了してしまうのでファイルfには変数は含まないようにした。
 
 問題6.3（教科書p.155）
 
@@ -50,10 +241,11 @@ process(CurrentTerm,Term) :- (naf(CurrentTerm=Term),!;write(CurrentTerm),nl),fin
     のすぐ前の空白はすべて削除し，各カンマのあとには1つの空白を
     入れねばならないとする．
 */
+squeeze :- get0(C),put(C),dorest(C).
+dorest(46) :- !.
+dorest(32) :- !,get(C),put(C),dorest(C).
+dorest(Letter) :- squeeze.
 
-accepts(S,[],_) :- final(S).
-accepts(S,[X|Rest],MaxMoves) :- MaxMoves>0,trans(S,X,S1),NewMax is MaxMoves - 1,accepts(S1,Rest).
-accepts(S,String,MaxMoves) :- MaxMoves>0,silent(S,S1),NewMax is MaxMoves - 1,accepts(S1,String,NewMax).
 
 /*（実行例）
 
